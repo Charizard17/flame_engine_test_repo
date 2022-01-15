040bbfd888d4aa0716e1142fb779402bffbeac93
Esad Dursun <3723esad@gmail.com>
2022-01-15 12:39:10 +0100

    flame sprite example

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# flame_game_red_square
+# flame_engine_test_repo
 
 [![Powered by Flame](https://img.shields.io/badge/Powered%20by-%F0%9F%94%A5-orange.svg)](https://flame-engine.org)
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
+
+void main() {
+  print('main file');
+
+  final myGame = MyGame();
+  runApp(
+    GameWidget(game: myGame),
+  );
+}
+
+class MyGame extends FlameGame {
+  SpriteComponent girl = SpriteComponent();
+
+  @override
+  Future<void> onLoad() async {
+    print('loading....');
+
+    girl
+      ..sprite = await loadSprite('girl.png')
+      ..size = Vector2(100.0, 100.0)
+      ..x = 150..y = 50;
+
+    add(girl);
+  }
+}
