b990e01a0385f0587d509ce285b2ff0a566c0b61
Esad Dursun <3723esad@gmail.com>
2022-01-15 16:16:11 +0100

    character movement added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/flame.dart';
 
@@ -12,18 +13,50 @@ void main() {
   );
 }
 
-class MyGame extends FlameGame {
+class MyGame extends FlameGame with DoubleTapDetector {
   SpriteComponent girl = SpriteComponent();
+  bool running = true;
+  String direction = 'down';
 
   @override
   Future<void> onLoad() async {
     print('loading....');
-
     girl
       ..sprite = await loadSprite('girl.png')
       ..size = Vector2(100.0, 100.0)
-      ..x = 150..y = 50;
-
+      ..x = 150
+      ..y = 50;
     add(girl);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    switch (direction) {
+      case 'down':
+        girl.y += 2;
+        break;
+      case 'up':
+        girl.y -= 2;
+        break;
+    }
+
+    if (girl.y > 400) {
+      direction = 'up';
+    }
+    if (girl.y < 20) {
+      direction = 'down';
+    }
+  }
+
+  @override
+  void onDoubleTap() {
+    if (running) {
+      pauseEngine();
+    } else {
+      resumeEngine();
+    }
+    running = !running;
   }
 }
