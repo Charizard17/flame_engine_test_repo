422062c76112e4a59934c689c41bfcff663e57ee
Esad Dursun <3723esad@gmail.com>
2022-01-15 16:40:53 +0100

    spritesheet image file added. sprite animation added

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class MyGame extends FlameGame with DoubleTapDetector {
   SpriteComponent girl = SpriteComponent();
   bool running = true;
   String direction = 'down';
+  SpriteAnimationComponent girlAnimation = SpriteAnimationComponent();
+  double speed = 2.0;
 
   @override
   Future<void> onLoad() async {
@@ -26,7 +28,18 @@ class MyGame extends FlameGame with DoubleTapDetector {
       ..size = Vector2(100.0, 100.0)
       ..x = 150
       ..y = 50;
-    add(girl);
+    // add(girl);
+
+    var spriteSheet = await images.load('girl_spritesheet.png');
+    final spriteSize = Vector2(152 * 1.4, 142 * 1.4);
+    SpriteAnimationData spriteData = SpriteAnimationData.sequenced(
+        amount: 9, stepTime: 0.05, textureSize: Vector2(151.3, 142.0));
+    girlAnimation =
+        SpriteAnimationComponent.fromFrameData(spriteSheet, spriteData)
+          ..x = 100
+          ..y = 50
+          ..size = spriteSize;
+    add(girlAnimation);
   }
 
   @override
@@ -35,17 +48,17 @@ class MyGame extends FlameGame with DoubleTapDetector {
 
     switch (direction) {
       case 'down':
-        girl.y += 2;
+        girlAnimation.y += speed;
         break;
       case 'up':
-        girl.y -= 2;
+        girlAnimation.y -= speed;
         break;
     }
 
-    if (girl.y > 400) {
+    if (girlAnimation.y > 400) {
       direction = 'up';
     }
-    if (girl.y < 20) {
+    if (girlAnimation.y < 20) {
       direction = 'down';
     }
   }
