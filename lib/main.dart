cbd013b2e5de2f4dbdceb703b642013e6d1ea8d2
Esad Dursun <3723esad@gmail.com>
2022-01-15 11:52:00 +0100

    all files deleted. starting new tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,1 @@
-import 'package:flame/game.dart';
-
-import './save_red_square.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  SaveRedSquare saveRedSquare = SaveRedSquare();
-
-  runApp(
-    GameWidget(
-      game: saveRedSquare,
-    ),
-  );
-}
--- lib/screens/screen_state.dart
+++
@@ -1,5 +0,0 @@
-enum ScreenState {
-  kMenuScreen,
-  kPlaygroundScreen,
-  kScoreScreen,
-}
